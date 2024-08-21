8150c77695e963e057f7883c7694eec9bd6dd5db
Raúl Álvarez <raulwii2@gmail.com>
2024-08-21 16:15:29 +0200

    Fix

--- services/AUTOMATIC1111/clone.sh
+++ services/AUTOMATIC1111/clone.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 set -Eeuox pipefail
+git config --global http.postBuffer 1048576000
+git config --global http.lowSpeedLimit 0
+git config --global http.lowSpeedTime 999999
 
-mkdir -p /repositories/"$1"
+MAX_ATTEMPTS=5
+COUNT=0
+
+until git clone "$2" /repositories/"$1" || [ $COUNT -eq $MAX_ATTEMPTS ]; do
+    COUNT=$((COUNT+1))
+    echo "Retrying clone ($COUNT/$MAX_ATTEMPTS)..."
+done
+
 cd /repositories/"$1"
-git init
-git remote add origin "$2"
-git fetch origin "$3" --depth=1
 git reset --hard "$3"
 rm -rf .git
